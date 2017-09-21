f5e3a8a9e8162d1cbb647c4c716e2bb098263c9d
Anri Liy <andy@anrinoimac.attlocal.net>
2017-09-21 08:29:57 -0500

    first git

---
+++ src/sample/models/HelloWorldModel.java
@@ -0,0 +1,14 @@
+package sample.models;
+
+public class HelloWorldModel {
+	private String message = "";
+
+	public void setMessage(String value){
+		this.message = value;
+	}
+
+	public String getMessage(){
+		return this.message;
+	}
+
+}
